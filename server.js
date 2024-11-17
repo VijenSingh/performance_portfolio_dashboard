c0d454f415f68a3b8e2cd3b6a8df322df068b48c
Vijendra Singh Panwar <46666066+VijenSingh@users.noreply.github.com>
2024-11-17 16:03:37 +0000

    update port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid'); 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 
